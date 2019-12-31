4e4545157d141cb74b61867708d2549cd32dffb0
etimoshenko <timoshenko.evgeny@gmail.com>
2019-12-31 22:36:11 +0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,40 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
 
-const IndexPage: NextPage = () => {
+const IndexPage: NextPage = props => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
+      <pre style={{ outline: "3px dotted pink" }}>
+        {JSON.stringify(props, null, 1)}
+      </pre>
       <p>
         <Link href="/about">
           <a>About</a>
         </Link>
       </p>
     </Layout>
-  )
+  );
+};
+
+// @ts-ignore
+export async function unstable_getStaticProps(params) {
+  console.log("getStaticProps", params);
+  return {
+    props: {
+      staticProp: params
+    }
+  };
 }
 
-export default IndexPage
+// export async function unstable_getStaticPaths() {
+//   return [
+//     // This renders / blog / hello-world to HTML at build time
+//     { params: { slug: "hello-world" } },
+//     { params: { slug: "dupa" } }
+//   ];
+// }
+
+export default IndexPage;
